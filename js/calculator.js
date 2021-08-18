529c3172ee0020434de5fdaf1a321baeed14b986
Samim Shammo <you@example.comsamimshammo>
2021-08-19 01:01:15 +0600

    first commit

---
+++ js/calculator.js
@@ -0,0 +1,36 @@
+// function getHistory(){
+//     const previus = document.getElementById('previus');
+//     const previusText = previus.innerText;
+//     return previusText;
+
+// }
+
+// function printHistory(num){
+//     const currentValue = document.getElementById('previus');
+//     const currentTextValue = currentValue.innerText= num;
+  
+// }
+// function getOuput(){
+//     const current = document.getElementById('current').innerText;
+//     return current
+// }
+// function printOoutput(num3){
+//      document.getElementById('current').innerText = formatNumber(num3);
+    
+// }
+// printOoutput(34234235)
+// function formatNumber(num3){
+//     let n=Number(num3);
+//     let value=n.toLocaleString('en')
+//     return value
+// }
+
+// let history;
+// let number = document.getElementsByClassName('container');
+// for(let i = 0; i< number.length; i++){
+//     number[i].addEventListener('click', function(){
+//         history = getHistory();
+//         history += this.id;
+//         printHistory(history);
+//     })
+// }
